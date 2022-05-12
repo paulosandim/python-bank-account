9236d3177fee4147c501459a51e2e9fdf9cbf258
paulo sandim <pauloedusandim@gmail.com>
2022-05-12 16:49:28 -0300

    create encapsulation

--- account.py
+++ account.py
@@ -4,11 +4,23 @@ class Account:
     
     def __init__(self, number, owner, balance, limit_account):
         print("Construct object ... {}".format(self))
-        self.number = number
-        self.owner = owner
-        self.balance = balance
-        self.limit_account = limit_account
+        self.__number = number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit_account = limit_account
         
         
     def statement(self):
-        print("Statement {} of owner {}".format(self.balance, self.owner))
+        print("Statement {} of owner {}".format(self.__balance, self.__owner))
+        
+    def deposit(self, value_account):
+        self.__balance += value_account
+        
+    def withdraw(self, value_account):
+        self.__balance += value_account
+        
+    def transfer(self, value_account, destiny):
+        self.withdraw(value_account)
+        destiny.deposit(value_account)
+        
+        
