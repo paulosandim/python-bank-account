72245c427c432d7202d276e661f1aa9bbc0bfb5b
paulo sandim <pauloedusandim@gmail.com>
2022-05-12 12:16:07 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,5 @@
+
+
+def creat_account(number, owner, balance, limit_account):
+    account = {"number": number, "owner account": owner, "balance": balance, "limit account": limit_account}
+    return account
