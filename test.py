7c3c0d06e569a3aaba780f683163a0786aa9f5b0
paulo sandim <pauloedusandim@gmail.com>
2022-05-12 12:43:33 -0300

    procedural

--- test.py
+++ test.py
@@ -3,3 +3,12 @@
 def creat_account(number, owner, balance, limit_account):
     account = {"number": number, "owner account": owner, "balance": balance, "limit account": limit_account}
     return account
+
+def deposit(account, value_account):
+    account["balance"] += value_account
+
+def withdraw(account, value_account):
+    account["balance"] -= value_account
+
+def statement(account):
+    print("Your balance is {}".format(account["balance"]))
