4a1e66add30104b8632f84eea0f7f3fd44a199b5
paulo sandim <pauloedusandim@gmail.com>
2022-05-12 17:39:38 -0300

    create propertys

--- account.py
+++ account.py
@@ -1,5 +1,4 @@
 
-
 class Account:
     
     def __init__(self, number, owner, balance, limit_account):
@@ -7,8 +6,7 @@ class Account:
         self.__number = number
         self.__owner = owner
         self.__balance = balance
-        self.__limit_account = limit_account
-        
+        self.__limit_account = limit_account        
         
     def statement(self):
         print("Statement {} of owner {}".format(self.__balance, self.__owner))
@@ -16,17 +14,26 @@ class Account:
     def deposit(self, value_account):
         self.__balance += value_account
         
+    def __can_withdraw(self, value_for_withdraw):
+        value_available = self.__balance + self.__limit_account
+        return value_for_withdraw <= value_available
+        
     def withdraw(self, value_account):
-        self.__balance += value_account
+        if(self.__can_withdraw(value_account)):
+            self.__balance -= value_account
+        else:
+            print("Value {} exceeded limit account".format(value_account))
         
     def transfer(self, value_account, destiny):
         self.withdraw(value_account)
         destiny.deposit(value_account)
-        
-    def get_balance(self):
+    
+    @property    
+    def balance(self):
         return self.__balance
     
-    def get_owner(self):
+    @property
+    def owner(self):
         return self.__owner
     
     @property
@@ -36,6 +43,13 @@ class Account:
     @limit_account.setter
     def limit_account(self, limit_account):
         self.__limit_account = limit_account
+        
+    @staticmethod
+    def bank_code():
+        return "001"
     
+    @staticmethod
+    def banks_codes():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
     
     
